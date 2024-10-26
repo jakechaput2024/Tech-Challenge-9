872820681df301e41f6a48813a20c31827b4ffe4
JAKE CHAPUT <JWC21@FSU.EDU>
2024-10-26 00:01:49 -0400

    dots on screen, numbered, and can draw lines between them.

--- sketch.js
+++ sketch.js
@@ -1,64 +1,45 @@
-// a simple game where you click to drop a ball and 
-// try to get as many balls as possible on the spinning shape
-// move the mouse up and down to change the rotation speed of the shape
-// use the right and left keys to make the shape larger or smaller
+var mouseprevX = 400;
+var mouseprevY = 500;
 
-let spinningShape;
-let highScore = 0;
+
+let star1, star2, star3, star4, star5, star6;
 
 function setup() {
-    let canvas = new Canvas("fullscreen");
+    new Canvas("fullscreen");
+    star1 = new Sprite(400, 500, 30);
+	star1.color = 'white';
+    star2 = new Sprite(700, 230, 25);
+	star2.color = 'white';
+    star3 = new Sprite(800, 300, 15);
+	star3.color = 'white';
+    star4 = new Sprite(900, 420, 35);
+	star4.color = 'white';
+    star5 = new Sprite(1000, 600, 50);
+	star5.color = 'white';
+    star6 = new Sprite(1300, 350, 36);
+    star6.color = 'white';
 
-    world.gravity.y = 10;
 
-    // this sprite can be created on a single line, but it's easier to read this way:
-	spinningShape = new Sprite();
-	spinningShape.width = canvas.width/5;
-	spinningShape.height = spinningShape.width;
-    spinningShape.collider = "kinematic";
-
-    textFont("Courier", 24);
+    stroke(250);
 }
 
 function draw() {
+    background(1, 20, 28);
+    noLoop();
 
-    // try the game without this line :)
-    clear();
 
-    // the map function translates a value from one range to another
-    // https://p5js.org/reference/#/p5/map
-    spinningShape.rotationSpeed = map(mouse.y, 0, canvas.height, -10, 10);
-
-    // create a ball when the mouse is clicked
-    // https://p5play.org/learn/input_devices.html
-    if (mouse.presses()) {
-        let ball = new Sprite(mouse.x, -20, 20);
-        // make the ball resist rolling when it touches the spinning shape
-        // https://p5play.org/learn/sprite.html?page=9
-        ball.rotationDrag = 10;
-    }
-
-    // make the spinning shape larger or smaller using the keyboard
-    // note the difference between presses (above) and pressing (here)
-    if (kb.pressing('right')) {
-        spinningShape.width += 10;
-    } else if (kb.pressing('left')) {
-        spinningShape.width -= 10;
-    }
-
-    // loop through the allSprites array and see how many are above the center of the screen
-    let currentScore = 0;
-    for (let sprite of allSprites) {
-        if (sprite.y < canvas.height/2) {
-            currentScore++;
-            if (currentScore > highScore) {
-                highScore = currentScore;
-            }
-        }
-    }
-
-    // display the score (minus 1 so it doesn't count the spinning shape)
-    text("BALLS: " + (currentScore-1), 40, 60);
-    text("HIGH:  " + (highScore-1), 40, 86);
-
+    textSize(12);
+    fill('white');
+    text('1', 400, 480);
+    text('2', 695, 210);
+    text('3', 795, 280);
+    text('4', 895, 400);
+    text('5', 1000, 570);
+    text('6', 1295, 320);
 }
+
+function mouseClicked() {
+    line(mouseprevX, mouseprevY, mouseX, mouseY);
+    mouseprevX = mouseX;
+    mouseprevY = mouseY;
+  }
