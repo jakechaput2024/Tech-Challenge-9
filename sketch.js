e36b57405943b49c0db11ee0e44b64caef15b0c4
JAKE CHAPUT <JWC21@FSU.EDU>
2024-10-27 01:55:24 -0400

    made an array for the pointsthat stores info about the points selected that'll have a line drawn between them

--- sketch.js
+++ sketch.js
@@ -1,5 +1,7 @@
-let star1, star2, star3, star4, star5, star6;
+let star1, star2, star3, star4, star5, star6; //white circles that show up in case 1
 let stage = 0;
+let linestartPoint = []; //this array stores info about the stars/circles clicked, forming a "connection" between them
+let selectedStar; 
 
 function setup() {
     new Canvas("fullscreen");
@@ -15,8 +17,7 @@ function setup() {
 	star5.color = 'white';
     star6 = new Sprite(1300, 350, 36);
     star6.color = 'white';
-
-
+    
     stroke(250);
 }
 
@@ -41,36 +42,50 @@ function draw() {
             }
             break;
         case 1:
-            //game
-            background(1, 20, 28);
-            noLoop();
-        
-        
-            textSize(12);
-            fill('white');
-            text('1', 400, 480);
-            text('2', 695, 210);
-            text('3', 795, 280);
-            text('4', 895, 400);
-            text('5', 1000, 570);
-            text('6', 1295, 320);
+                //game
+                background(1, 20, 28);
+                
+                textSize(12);
+                fill('white');
+                text('1', 400, 480);
+                text('2', 695, 210);
+                text('3', 795, 280);
+                text('4', 895, 400);
+                text('5', 1000, 570);
+                text('6', 1295, 320);
 
-            star1.visible = true;
-            star2.visible = true;
-            star3.visible = true;
-            star4.visible = true;
-            star5.visible = true;
-            star6.visible = true;
-            break;
+                star1.visible = true;
+                star2.visible = true;
+                star3.visible = true;
+                star4.visible = true;
+                star5.visible = true;
+                star6.visible = true;
+                
+                for (let linepoints of linestartPoint) {
+                    let l1 = linepoints[0];
+                    let l2 = linepoints[1];
+                    line(l1.x, l1.y, l2.x, l2.y);
+                }
+
+                if (selectedStar) {
+                    line(startCircle.x, startCircle.y, mouseX, mouseY);
+                }
+
+                if (mouse.presses()) {
+                    if (dist(mouseX, mouseY, star1.x, star1.y) < 10) {
+                        handleCircleClick(star1);
+                      } else if (dist(mouseX, mouseY, star2.x, star2.y) < 10) {
+                        handleCircleClick(star2);
+                      } else if (dist(mouseX, mouseY, star3.x, star3.y) < 10) {
+                        handleCircleClick(star3);
+                      } else {
+                        selectedStar = null;  // Deselect if clicking outside any circle
+                      }
+                }
+                break;
         case 2:
-            //congrats screen
-            break;
+                //congrats screen
+                break;
     }
-   
-}
 
-function mouseClicked() {
-    line(mouseprevX, mouseprevY, mouseX, mouseY);
-    mouseprevX = mouseX;
-    mouseprevY = mouseY;
-  }
+}
